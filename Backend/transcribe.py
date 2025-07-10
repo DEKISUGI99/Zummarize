3057cd868f6415bd261a5f674a4fe2fdf2196aea
DEKISUGI99 <tanishqtank996@gmail.com>
2025-07-10 11:30:56 +0530

    Intergrated whisper transcription

--- Backend/transcribe.py
+++ Backend/transcribe.py
@@ -3,6 +3,5 @@ import whisper
 model = whisper.load_model("base")  # You can use "small" or "medium" if needed
 
 def transcribe_audio(audio_path):
-    result = model.transcribe(audio_path, language=None)
-    print("Detected language:", result.get("language"))
+    result = model.transcribe(audio_path)
     return result["text"]
